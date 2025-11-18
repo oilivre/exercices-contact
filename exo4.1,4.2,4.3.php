1fcd397be37600aa2432e5ece5b78ae40aab7a74
oilivre <olivieramar77@gmail.com>
2025-11-18 15:22:15 +0100

    Update exo4.1,4.2,4.3.php

--- exo4.1,4.2,4.3.php
+++ exo4.1,4.2,4.3.php
@@ -35,23 +35,23 @@ function strcontain($string, $chaine){
 
 echo strcontain("ABC", "ABCEFG")*/
 
-$contact = fopen("table.txt", "r");
-$status =1;
-while(!feof($contact)){
+
+$contacte = fopen("table.txt", "r");
+$status = 1;
+while(!feof($contacte)){
     if($status == 1){
         $status++;
-    } else {
-    $line = fgets($contact);
-    $pieces = explode(" ", $line) ;
-    for ($i=0; $i<=count($pieces)-1 ;$i++){
-        if($i=!0 && $piece[0]*$i ==$piece[$i]){
-            echo $piece[0]*$i;
-        } else {
-            echo  $piece[$i];
-        }
+        print_r(fgets($contacte));
+    }else{
+        $ligne = explode(" ", fgets($contacte));
+        for ($i = 0; $i<=count($ligne)-1 ; $i++){
+            if($i != 0 && $ligne[0]*$i !=$ligne[$i]){
+                print_r($ligne[0]*$i. " ");
+            }else{
+                print_r($ligne[$i]." ");
+            }}
+        echo "<br>";
     }
-    }
-    echo "<br>";
-}
-
+};
+ 
 ?>
